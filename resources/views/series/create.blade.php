b0d5fbfeb5ecdd906c6e294f48e8de41a6da1fa3
Eduardo Alexandre <eduardoalexdev@gmail.com>
2024-03-27 10:27:18 -0300

    feat: create series controller

---
+++ resources/views/series/create.blade.php
@@ -0,0 +1,6 @@
+<x-layout title="Create Serie">
+    <form action="" method="post">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name">
+    </form>
+</x-layout>
